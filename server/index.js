a66beb690dad26ff0253dac3bb872e3f469aa283
whygra <why.grabovsky@gmail.com>
2022-06-18 00:19:11 +0300

    .env

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
@@ -6,15 +8,15 @@ import cors from 'cors'
 import postsRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 app.use('/posts', postsRoutes)
 
-const CONNECTION_URL = "mongodb+srv://mastermind:YNN9Eu$ZUAha2z_@cluster0.haaup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
     .catch((error) => console.log(error.message))
